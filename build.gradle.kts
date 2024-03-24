2e0b977085fa5f102ff5b08df8a578faf9ec520f
jathevan <jathevan15@gmail.com>
2024-03-24 16:32:13 +1000

    Add application plugin and configure main class for Kafka producer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -12,7 +13,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
     implementation ("org.apache.kafka:kafka-clients:3.6.0")
+}
+
+application {
+    mainClass.set("org.example.KafkaProducerHello")
 }
 
 tasks.test {
