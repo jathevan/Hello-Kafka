1d5d65a3be6db61f1c3b10553a61baba8af09059
jathevan <jathevan15@gmail.com>
2024-03-24 13:06:17 +1000

    Update JUnit BOM to 5.10.1 and add Kafka clients dependency
    * Changed testImplementation(platform("org.junit:junit-bom:5.9.1")) to testImplementation(platform("org.junit:junit-bom:5.10.1"))
    * Added implementation ("org.apache.kafka:kafka-clients:3.6.0")

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("org.apache.kafka:kafka-clients:3.6.0")
 }
 
 tasks.test {
